9aadde5952a69d0e08e85c06facc981b2ba823bd
Evangelina Fernandez <fernandez.eva.tdf@gmail.com>
2022-06-27 13:22:56 -0300

    configuracion

---
+++ api/db/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/turnon", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then((db) => console.log("database is connected"))
+  .catch((err) => console.log(err));
