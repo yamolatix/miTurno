a5af9286f949adb9b48554459a576d56490ce9cc
Matias Jaliff <matiasjaliff@gmail.com>
2022-06-29 19:41:15 -0300

    actualización 29-jun

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import  store  from './app/store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
