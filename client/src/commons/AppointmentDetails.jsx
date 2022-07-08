f393952dbea670e5c5f9b6e4c44128ea50d118e4
DevNachoGarcia <igarciariestra@gmail.com>
2022-07-07 22:06:32 -0300

    merge

--- client/src/commons/AppointmentDetails.jsx
+++ client/src/commons/AppointmentDetails.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import style from "../styles/Users.module.css";
 
-const AppointmentDetails = ({ branchOffice, date, time }) => {
+const AppointmentDetails = () => {
 
-    console.log('SUCURSAL ES ', branchOffice)
+    /* console.log('SUCURSAL ES ', branchOffice)
     console.log('DATE ES ', date)
-    console.log('TIME ES ', time)
+    console.log('TIME ES ', time) */
 
-    branchOffice = {
+    /* branchOffice = {
         location: "retiro",
         address: "Av. Illia 4651",
         phone: 1147854211,
@@ -19,17 +19,19 @@ const AppointmentDetails = ({ branchOffice, date, time }) => {
         simultAppointment: 5,
         price: 700,
         id: 5
-    }
+    } */
 
-    time = "12:00"
+    //time = "12:00"
+    
 
-  return time ? (
+  /* return time ? (
     <div className={style.userDetails}>
       <h5>Detalle del turno</h5>
       <ul>
         <li>Sucursal: {branchOffice.location.toUpperCase()}</li>
         <li>Dirección: {branchOffice.address}</li>
         <li>Teléfono: {branchOffice.phone}</li>
+        <li>Email: {branchOffice.email}</li>
         <li>Fecha: {date}</li>
         <li>Hora: {time}</li>
         <li>Precio: ${branchOffice.price}</li>
@@ -38,18 +40,22 @@ const AppointmentDetails = ({ branchOffice, date, time }) => {
         <>
           <Button
             variant="secondary"
-            className={style.sideButton}
+            className={style.sideButton} */
+
             /* onClick={() =>
               handleRoleChange(user._id, user.admin, user.operator)
             } */
-          >
+
+          /* >
             Reservar
           </Button>
           <Button
             variant="secondary"
-            className={style.sideButton}
+            className={style.sideButton} */
+
             //onClick={() => handleDelete(user._id)}
-          >
+
+        /*   >
             Cancelar
           </Button>
         </>
@@ -57,7 +63,7 @@ const AppointmentDetails = ({ branchOffice, date, time }) => {
     </div>
   ) : (
     <></>
-  );
+  ); */
 };
 
 export default AppointmentDetails;
