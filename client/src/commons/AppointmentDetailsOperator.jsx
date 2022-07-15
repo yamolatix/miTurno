47bfee50e82c5f02e061db279815fbae20a700a0
DevNachoGarcia <igarciariestra@gmail.com>
2022-07-15 07:24:05 -0300

    viernes tempra2

--- client/src/commons/AppointmentDetailsOperator.jsx
+++ client/src/commons/AppointmentDetailsOperator.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 import { useSelector } from "react-redux";
 import style from "../styles/Users.module.css";
 import { getFullDate } from "../utils/getFullDate";
@@ -12,41 +13,66 @@ const AppointmentDetailsOperator = () => {
   const user = parseJwt(JSON.parse(localStorage.getItem('user')).data.token)
 
   const pickedDate = useSelector(state => state.appointment)
-  const pickedBranchOffice = useSelector(state => state.branchOffice.clickedOffice)
-  const [appointmentUsers, setAppointmentUsers] = useState([])
-  //const branchOffices = JSON.parse(localStorage.getItem('branches')).branches
 
-  /* const asignedOffice = branchOffices.find(branch => 
-    user.branchOffice.includes(branch._id))
-  console.log('LA SUCURSAL A SETEAR ES ', asignedOffice) */
+  const pickedBranchOffice = useSelector(state => state.branchOffice.clickedOffice)
+
   
+  const fakeUsers = [
+    {
+      lname: 'Moura',
+      fname: 'Federico',
+      phone: '0303456',
+      email: 'virus@mail.com'
+    },
+    {
+      lname: 'Aznar',
+      fname: 'Pedro',
+      phone: '3415897456',
+      email: 'musiquita@mail.com'
+    },
+    {
+      lname: 'Abuelo',
+      fname: 'Miguel',
+      phone: '01145789143',
+      email: 'delanada@mail.com'
+    }
+  ]
+  
+  const [appointmentUsers, setAppointmentUsers] = useState(fakeUsers)
+
   // VER URL DE ACA ABAJO !!!!!!!!
   const getAppointmentUsers = () => {
-    axios.get(`http://localhost:3001/api/appointment/${user.id}/dayAppointments`, {
-      headers: {
-        date: pickedDate.date,
-        month: pickedDate.month,
-        year: pickedDate.year,
-        time: getFixedTime(pickedDate),
-        id: pickedBranchOffice.id
-      }
-    })
-    .then(arr => {
-      console.log('USUARIOS CON ESTE TURNO SON ', arr.data.data)
-      setAppointmentUsers(arr.data.data)
-    })
-    .catch(err => console.log(err))
-  }
+    if (pickedBranchOffice && pickedDate) {
+      axios.get(`http://localhost:3001/api/appointment/${user.id}/dayAppointments`, {
+        headers: {
+          date: pickedDate.date,
+          month: pickedDate.month,
+          year: pickedDate.year,
+          time: getFixedTime(pickedDate),
+          id: pickedBranchOffice.id
+        }
+      })
+      .then(arr => {
+        console.log('USUARIOS CON ESTE TURNO SON ', arr.data.data)
+        setAppointmentUsers(arr.data.data)
+      })
+      .catch(err => console.log(err))
+    };
+  };
 
-  const handleAssitance = () => {
+  const handleAssitance = (appointment) => {
     /* axios.put(`http://localhost:3001/api/appointment/${user.id}/showAppointments`, {
-      id: 
+      id: (appointmentId)
   })
-    .then */
+    .then(() => {
+      appointmentUsers.splice(appointmentUsers.indexOf(appointment), 1)
+      Report.success('miTurno', 'Se confirmó la asistencia del usuario', 'Ok');
+    })
+    .catch(err => Report.failure('miTurno', {err}, 'Ok')) */
 }
 
   useEffect(()=> {
-    getAppointmentUsers()
+    //getAppointmentUsers()
   },[pickedDate])
 
   return pickedDate.date ? (
@@ -60,21 +86,25 @@ const AppointmentDetailsOperator = () => {
       {appointmentUsers.length
       ? (
         <>
-        <h5>Usuarios agendados:</h5>
+        <h5 className={style.agendados}> Usuarios agendados: </h5>
         <ul>
           {appointmentUsers.map(e => (
-            <>  
-            <li> {e.lname.toUpperCase()}, {e.fname.toUpperCase()}</li>
-            <li> Teléfono: {e.phone}</li>
-            <li> Email: {e.email}</li>
-            <Button
-              variant="secondary"
-              className={style.sideButton}
-              onClick={() => handleAssitance()}
-            >
-              Asistió
-            </Button>
-            {/* agregar para marcar si asistió o no */}
+            <>
+              <div className={style.asistentes}>  
+                <li> {e.lname.toUpperCase()}, {e.fname.toUpperCase()}</li>
+                <li> Teléfono: {e.phone}</li>
+                <li> Email: {e.email}</li>
+                <Button
+                  variant="secondary"
+                  className={style.sideButton}
+                  onClick={(appointment) => {
+                    //handleAssitance(appointment)
+                    }
+                  }
+                >
+                  Asistió
+                </Button>
+              </div>
             </>
             )
           )}  
@@ -99,4 +129,4 @@ export default AppointmentDetailsOperator;
 router.put("/:operatorId/showAppointments", async (req, res) => {
   const { operatorId } = req.params;
   const appointmentId = req.body.id;
-  /*
+  */
